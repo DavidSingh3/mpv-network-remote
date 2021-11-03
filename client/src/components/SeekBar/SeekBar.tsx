84007f64d9dfefcaad6e556de0c2719e524a66f5
david <david@appsaloon.be>
2021-11-03 21:19:40 +0100

    Fix: reduced REST spam caused by the change event listener of the SeekBar component's slider element.

--- client/src/components/SeekBar/SeekBar.tsx
+++ client/src/components/SeekBar/SeekBar.tsx
@@ -4,8 +4,9 @@ import useMpvInformation from '../../hooks/useMpvInformation'
 import { MdPlayArrow, MdPause, MdStop, MdCloseFullscreen, MdFullscreen } from 'react-icons/md'
 import classes from './SeekBar.module.scss'
 import { setTimePosition, togglePause, stop, toggleFullscreen } from '../../util/mpvCommands'
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import useSocketIO from '../../hooks/useSocketIO'
+import useDelay from '../../hooks/useDelay'
 
 function formatSeconds (seconds: number) {
   const duration = intervalToDuration({ start: 0, end: seconds * 1_000 })
@@ -21,12 +22,26 @@ export default function SeekBar (props: {file: MPVFile}) {
   const timePassedHumanReadable = formatSeconds(props.file.timePosition)
   const durationHumanReadable = formatSeconds(props.file.duration)
   const socket = useSocketIO()
+  const [controllerTimePosition, delayedControllerTimePosition, setControllerTimePosition] = useDelay<number>(props.file.timePosition)
+  const [displayTimePosition, setDisplayTimePosition] = useState(props.file.timePosition)
 
   function handleChangeSlider (event: ChangeEvent<HTMLInputElement>) {
-    setTimePosition(parseInt(event.target.value ?? 0, 10)).then(() => {
+    setControllerTimePosition(parseInt(event.target.value ?? 0, 10))
+  }
+
+  useEffect(function () {
+    if (controllerTimePosition !== delayedControllerTimePosition) {
+      setDisplayTimePosition(controllerTimePosition)
+    } else {
+      setDisplayTimePosition(props.file.timePosition)
+    }
+  }, [controllerTimePosition, delayedControllerTimePosition, props.file.timePosition])
+
+  useEffect(function () {
+    setTimePosition(delayedControllerTimePosition).then(() => {
       socket.emit('mpv-property-change-request')
     })
-  }
+  }, [delayedControllerTimePosition, socket])
 
   return <nav className={classes.seekBar}>
     <button className={classes.playButton} onClick={togglePause}>
@@ -55,8 +70,8 @@ export default function SeekBar (props: {file: MPVFile}) {
         type="range"
         min="0"
         max={props.file.duration}
-        value={props.file.timePosition}
-        onChangeCapture={handleChangeSlider}
+        value={displayTimePosition}
+        onChange={handleChangeSlider}
         id="myRange"
     />
   </nav>
