3ae41e25b982caaef11397c6fac789a7cf5eec09
david <david@appsaloon.be>
2021-11-03 21:12:10 +0100

    Refact: made useDelay hook optionally generic

--- client/src/hooks/useDelay.ts
+++ client/src/hooks/useDelay.ts
@@ -1,9 +1,9 @@
 import { Dispatch, useEffect, useState } from 'react'
 
-export default function useDelay (initialState: string): [string, string, Dispatch<string>] {
-  const [value, setValue] = useState<string>(initialState)
-  const [delayedValue, setDelayedValue] = useState<string>(initialState)
-  const [timer, setTimer] = useState<{id: number, value: string}|null>(null)
+export default function useDelay<Type=string> (initialState: Type): [Type, Type, Dispatch<Type>] {
+  const [value, setValue] = useState<Type>(initialState)
+  const [delayedValue, setDelayedValue] = useState<Type>(initialState)
+  const [timer, setTimer] = useState<{id: number, value: Type}|null>(null)
 
   useEffect(() => {
     if (timer?.value === value) {
@@ -11,17 +11,13 @@ export default function useDelay (initialState: string): [string, string, Dispat
     }
     clearTimeout(timer?.id)
 
-    if (value.length) {
-      const id = window.setTimeout(() => {
-        setDelayedValue(value)
-      }, 500)
-      setTimer({
-        id,
-        value
-      })
-    } else {
-      setDelayedValue('')
-    }
+    const id = window.setTimeout(() => {
+      setDelayedValue(value)
+    }, 500)
+    setTimer({
+      id,
+      value
+    })
   }, [value, timer])
 
   return [value, delayedValue, setValue]
