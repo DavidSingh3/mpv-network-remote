3a325d82d46fdfc9580920544cd1860f02519c4d
david <david@appsaloon.be>
2021-11-06 04:02:20 +0100

    Refact: added short delay before showing the loading Spinner component

--- client/src/components/Spinner/Spinner.tsx
+++ client/src/components/Spinner/Spinner.tsx
@@ -1,13 +1,29 @@
 import { ImSpinner9 } from 'react-icons/im'
 import classes from './Spinner.module.scss'
 import { createPortal } from 'react-dom'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { TasksContext } from '../TasksContextManager/TasksContextManager'
+import useBooleanState from '../../hooks/useBooleanState'
 
 export default function Spinner () {
   const { tasks } = useContext(TasksContext)
+  const [showSpinner, flipOrSetShowSpinner] = useBooleanState(false)
 
-  return tasks.length
+  useEffect(() => {
+    if (tasks.length) {
+      const timeout = window.setTimeout(() => {
+        flipOrSetShowSpinner(true)
+      }, 300)
+
+      return () => {
+        window.clearTimeout(timeout)
+      }
+    } else {
+      flipOrSetShowSpinner(false)
+    }
+  }, [flipOrSetShowSpinner, tasks.length])
+
+  return showSpinner
     ? createPortal(
       <div className={classes.spinner}>
         <ImSpinner9 className={classes.icon}/>
